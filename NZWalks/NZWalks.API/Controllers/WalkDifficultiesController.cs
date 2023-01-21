a39f657b095d1c1cb1bbccb6dfc2925a7a69d83f
jinosh <jinoshkurisinkal@hotmail.com>
2023-01-21 19:49:06 +0530

    Added fluent validations for Region request

---
+++ NZWalks/NZWalks.API/Controllers/WalkDifficultiesController.cs
@@ -0,0 +1,112 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Repository;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalkDifficultiesController : ControllerBase
+    {
+        private readonly IWalkDifficultyRepository walkDifficultyRepository;
+        private readonly IMapper mapper;
+
+        public WalkDifficultiesController(IWalkDifficultyRepository walkDifficultyRepository, IMapper mapper)
+        {
+            this.walkDifficultyRepository = walkDifficultyRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWalkDifficulties()
+        {
+            var walkDifficultiesDomain = await walkDifficultyRepository.GetAllAsync();
+
+            // Convert Domain to DTOs
+            var walkDifficultiesDTO = mapper.Map<List<Models.DTO.WalkDifficultyDTO>>(walkDifficultiesDomain);
+
+            return Ok(walkDifficultiesDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetWalkDifficultyById")]
+        public async Task<IActionResult> GetWalkDifficultyById(Guid id)
+        {
+            var walkDifficulty = await walkDifficultyRepository.GetAsync(id);
+            if (walkDifficulty == null)
+            {
+                return NotFound();
+            }
+
+            // Convert Domain to DTOs
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficultyDTO>(walkDifficulty);
+
+            return Ok(walkDifficultyDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWalkDifficultyAsync(
+            Models.DTO.AddWalkDifficultyRequest addWalkDifficultyRequest)
+        {
+            // Convert DTO to Domain model
+            var walkDifficultyDomain = new Models.Domain.WalkDifficulty
+            {
+                Code = addWalkDifficultyRequest.Code
+            };
+
+            // Call repository
+            walkDifficultyDomain = await walkDifficultyRepository.AddAsync(walkDifficultyDomain);
+
+            // Convert Domain to DTO
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficultyDTO>(walkDifficultyDomain);
+
+            // Return response
+            return CreatedAtAction(nameof(GetWalkDifficultyById),
+                new { id = walkDifficultyDTO.Id }, walkDifficultyDTO);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateWalkDifficultyAsync(Guid id,
+            Models.DTO.UpdateWalkDifficultyRequest updateWalkDifficultyRequest)
+        {
+            // Convert DTO to Domiain Model
+            var walkDifficultyDomain = new Models.Domain.WalkDifficulty
+            {
+                Code = updateWalkDifficultyRequest.Code
+            };
+
+            // Call repository to update
+            walkDifficultyDomain = await walkDifficultyRepository.UpdateAsync(id, walkDifficultyDomain);
+
+            if (walkDifficultyDomain == null)
+            {
+                return NotFound();
+            }
+
+            // Convert Domain to DTO
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficultyDTO>(walkDifficultyDomain);
+
+            // Return response
+            return Ok(walkDifficultyDTO);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteWalkDifficulty(Guid id)
+        {
+            var walkDifficultyDomain = await walkDifficultyRepository.DeleteAsync(id);
+            if (walkDifficultyDomain == null)
+            {
+                return NotFound();
+            }
+
+            // Convert to DTO
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficultyDTO>(walkDifficultyDomain);
+            return Ok(walkDifficultyDTO);
+        }
+
+    }
+}
--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -1,3 +1,4 @@
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
 using NZWalks.API.Repository;
@@ -11,16 +12,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services
+        .AddFluentValidation(options => options.RegisterValidatorsFromAssemblyContaining<Program>());
 builder.Services.AddDbContext<NZWalksDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks"));
 });
 builder.Services.AddScoped<IRegionRepository,RegionRepository>();
 builder.Services.AddScoped<IWalkRepository, WalkRepository>();
+builder.Services.AddScoped<IWalkDifficultyRepository, WalkDifficultyRepository>();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddControllersWithViews()
                 .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
